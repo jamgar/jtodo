f257217f4c7d60cc4b0d6d9126142fdd41bce28e
James <jegpub@gmail.com>
2017-05-29 14:55:45 -0500

    Update Task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,7 +33,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to tasks_path, notice: 'Task was successfully updated.' }
       else
         format.html { render :edit }
       end
